7216ffcf41f9c0a66e01bca22ed04b3c1040c34c
Brody Wilson <69532317+wilsonERR@users.noreply.github.com>
2023-02-09 21:14:40 -0600

    removed locale comments from Main

--- src/main/java/mwilson/fxschedule/Main.java
+++ src/main/java/mwilson/fxschedule/Main.java
@@ -7,12 +7,10 @@ import javafx.stage.Stage;
 import mwilson.fxschedule.Database.DBConnection;
 
 import java.io.IOException;
-import java.util.Locale;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        //Locale.setDefault(new Locale("fr", "FR"));
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LogIn.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Schedule FX 3000");
